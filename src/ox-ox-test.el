6f3c952f2429db3666b9040e3cfef56529db223f
Corwin Brust <corwin@bru.st>
2020-03-22 00:06:12 -0500

    rx support for remote()

--- src/ox-ox-test.el
+++ src/ox-ox-test.el
@@ -36,81 +36,91 @@
 	(col-sigel ?$)
 	(row-sigel ?@)
 	(key-sigel ?:)
-	(named (group (seq (any "[a-zA-Z_]")
-			   (0+ (any alnum ?_)))))
-	(numbered (group (1+ digit)))
+	(comma (and (0+ blank) ?, (0+ blank)))
 	(sign (group (any ?+ ?-)))
 	(boundry (group (1+ (or ?< ?>))))
-	(special (or (seq var-sigel (group (any ?* ?_ ?: ?#)) (opt key))
-		     (seq ?@ (group ?#))))
-	;;(nonzero (seq "[1-9]" (1+ "[0-9]")))
-	(key (seq key-sigel named))
+	(numbered (group (1+ digit)))
+	(named (group (and (any "a-z" "A-Z" "_")
+			   (0+ (any alnum ?_)))))
+	(key (and key-sigel named))
+	(special (or (and var-sigel (group (any ?* ?_ ?: ?#)) (opt key))
+		     (and ?@ (group ?#))))
+	(remote (item) (and "remote(" (0+ blank) (group (1+ (not ?,)))
+			    comma item ")"))
 	(col (or named
 		 boundry
-		 (seq (opt sign) numbered)))
+		 (and (opt sign) numbered)))
 	(row (or boundry
-		 (seq (opt sign)
+		 (and (opt sign)
 		      (group (1+ ?I))
-		      (opt (seq sign numbered)))
-		 (seq (opt sign) numbered)))
-	(cell (or (seq row-sigel row (opt (seq col-sigel col)))
-		  (seq col-sigel col)))
-	(range (seq cell (opt (seq ".." (or cell row)))))
-	(var (or special range))))
+		      (opt (and sign numbered)))
+		 (and (opt sign) numbered)))
+	(cell (or (and row-sigel row (opt (and col-sigel col)))
+		  (and col-sigel col)))
+	(range (and cell ".." (or cell row)))
+	(range? (and cell (opt (and ".." (or cell row)))))
+	(var (or special range? (remote range)))))
 ;; (eval (macroexpand `(rx-let ,my-rx (string-match-p (rx var) "$foo"))))
 
-(defvar my:labels nil "Labels for the groups of var in `my:rx'.")
-(setq my:labels '(full-match ; 0
-		  special-name ; 1
-		  special-keyword ; 2
-		  row-count ; 3
-		  row-boundry ; 4
-		  row-hline-sign ; 5
-		  row-hline ; 6
-		  row-hline-adj-sign ; 7
-		  row-hline-adj ; 8
-		  row-sign ; 9
-		  row-number ; 10
-		  field-name ; 11
-		  field-boundry ; 12
-		  field-sign ; 13
-		  field-number ; 14
-		  col-name ; 15
-		  col-boundry ; 16
-		  col-sign ; 17
-		  col-number ; 18
-		  right-row-boundry ; 19
-		  right-row-hline-sign ; 20
-		  right-row-hline ; 21
-		  right-row-hline-adj-sign ; 22
-		  right-row-hline-adj ; 23
-		  right-row-sign ; 24
-		  right-row-number ; 25
-		  right-field-name ; 26
-		  right-field-boundry ; 27
-		  right-field-sign ; 28
-		  right-field-number ; 29
-		  right-col-name ; 30
-		  right-col-boundry ; 31
-		  right-col-sign ; 32
-		  right-col-number ; 33
-		  ))
+(defvar my:rx-labels nil "Labels for the groups of var in `my:rx'.")
+(setq my:rx-labels
+      '(full-match ; 0
+	special-name ; 1
+	special-keyword ; 2
+	row-count ; 3
+	row-boundry ; 4
+	row-hline-sign ; 5
+	row-hline ; 6
+	row-hline-adj-sign ; 7
+	row-hline-adj ; 8
+	row-sign ; 9
+	row-number ; 10
+	field-name ; 11
+	field-boundry ; 12
+	field-sign ; 13
+	field-number ; 14
+	col-name ; 15
+	col-boundry ; 16
+	col-sign ; 17
+	col-number ; 18
+	right-row-boundry ; 19
+	right-row-hline-sign ; 20
+	right-row-hline ; 21
+	right-row-hline-adj-sign ; 22
+	right-row-hline-adj ; 23
+	right-row-sign ; 24
+	right-row-number ; 25
+	right-field-name ; 26
+	right-field-boundry ; 27
+	right-field-sign ; 28
+	right-field-number ; 29
+	right-col-name ; 30
+	right-col-boundry ; 31
+	right-col-sign ; 32
+	right-col-number ; 33
+	nil nil nil nil nil nil nil ;; 34-40
+	remote-name ; 41
+	))
 
-(defvar my:test-strings nil "These are test strings that should all match.")
-(setq my:test-strings '("$foo" "$foo:kw"
+(defvar my:rx-test-strings nil "These are test strings that should all match.")
+(setq my:rx-test-strings
+      '("$foo" "remote(Bar,$foo)" "$foo:kw" ;;ZZZ: ?
 	"$_" "$_:kw" "$*" "$*:kw" "$:" "$::kw"
-	;; "@#" "$#"
-	"@#" "$#"
-	"$1" "$+2" "$-3" "$>" "$>>>" "$<" "$<<"
-	"@1$1" "@-2$2" "@3$-3" "@+4$+4" "@+4$foo"
-	"@<$5" "@6$>" "@>>$7" "@8$<<" "@>>>>>$foo"
-	"@I$9" "@II$10" "@I$-1" "@I+1$-2" "@I$+1" "@I-1$+2"
-	"@III-3$11" "@+I$12" "@-II$13" "@+III-17$14"
-	"@1$2..@3$4" "@+1$-2..@-3$+4"
-	"@+II-2$foo..@<<$bar" "@>>>>>$-4..@-IIIII$bar:kw"
-	"@>$<..@+III-17$14" "@+III-17..$<" "@+III-17..$+14"))
+	"@#" "$#" "$1" "$+2" "$>>>"
+	;; "@1$1" "@-2$2" "@3$-3" "@+4$+4" "@+4$foo"
+	;; "@<$5" "@6$>" "@>>$7" "@8$<<" "@>>>>>$foo"
+	;; "@I$9" "@II$10" "@I$-1" "@I+1$-2" "@I$+1" "@I-1$+2"
+	;; "@III-3$11" "@+I$12" "@-II$13" "@+III-17$14"
+	;; "@1$2..@3$4" "@+1$-2..@-3$+4"
+	;; "@+II-2$foo..@<<$bar" "@>>>>>$-4..@-IIIII$bar:kw"
+	;; "@>$<..@+III-17$14" "@+III-17..$<" "@+III-17..$+14"
+	"@>$<..@+III-17$foo" "remote(Bar,@>$<..@+III-17$foo)"
+	"@+III-17$foo..$<" "remote(Bar,@+III-17$foo..$<)"
+	",@+2$foo..@<<<$+14" "remote(Bar,@+2$foo..@<<<$+14)"
+	"@>>$foo..$+42"	"remote(Bar,@>>$foo..$+42)"
+	"@+II-2$<..$foo" "remote(Bar,@+II-2$<..$foo)"))
 
-(defmacro def-rx-var-test (str &optional result)
+(defmacro my:rx-test (str &optional result)
   "Create an `ert' function testing STR expecting RESULT."
   (declare (indent 2))
   (ignore result)
@@ -121,9 +131,11 @@
 	(delq nil (seq-map-indexed
 		   (lambda (_ n)
 		     (when-let ((m (match-string n ,str))
-				(g (nth-value n my:labels)))
+				(g (or (nth-value (if (> n 41) (- n 38) n)
+						  my:rx-labels)
+				       "?")))
 		       (format "| %s | %s | %s |" n m g)))
-		   (make-list 34 nil)))
+		   (make-list 200 nil)))
 	"\n")
        )))
 
@@ -145,9 +157,9 @@ Try ~occur~ where ~n~ is a group number in:
     (print (eval `(macroexpand (rx-let ,my:rx (rx var))))
 	   (current-buffer))
     (insert "#+end_src\n\n* Test Strings")
-    (dolist (test-form my:test-strings)
+    (dolist (test-form my:rx-test-strings)
       (insert (format "\n\n** %s\n\n| n  | match | group |\n" test-form))
-      (insert (def-rx-var-test test-form)))
+      (insert (my:rx-test test-form)))
     (pop-to-buffer (current-buffer))
     (org-mode)))
 
